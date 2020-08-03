def78aaec220d3a7f3d493f4c5dba6e91a1074aa
mvximenko <mr.mxmnk@gmail.com>
2020-08-03 16:33:41 +0900

    Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    background: '#282828',
+  },
+  title: {
+    width: '100%',
+    textAlign: 'center',
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position='static' className={classes.appBar}>
+        <Toolbar>
+          <Typography variant='h6' className={classes.title}>
+            Welcome 🦊
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
